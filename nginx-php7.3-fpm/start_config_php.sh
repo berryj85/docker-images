a55698c5211c2861dbe8b829f8e82f4f3f86f803
Janepop Atirattanachai <jane3030@gmail.com>
2020-06-09 13:13:18 +0700

    replace session save_path when set session handler is redis

--- nginx-php7.3-fpm/start_config_php.sh
+++ nginx-php7.3-fpm/start_config_php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$STORE_SESSION" = redis ] ; then
     sed -i -e "s/session\.save_handler\ =\ files/session\.save_handler\ =\ redis/g" /etc/php/7.3/fpm/php.ini
-    sed -i -e "s/\;session\.save_path\ =\ \"\/var\/lib\/php\/sessions\"/session\.save_path\ =\ \"tcp:\/\/\\$REDIS_HOST\"/g" /etc/php/7.3/fpm/php.ini
+    sed -i -e "s/\;session\.save_path\ =\ \"\/var\/lib\/php\/sessions\"/session\.save_path\ =\ \"tcp:\/\/$REDIS_HOST\"/g" /etc/php/7.3/fpm/php.ini
     
     cat /etc/php/7.3/fpm/php.ini | grep session.save_handler
     cat /etc/php/7.3/fpm/php.ini | grep session.save_path
