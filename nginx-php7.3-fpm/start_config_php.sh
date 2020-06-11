5c80e99d96fe9ea28336fce67913de7f3fd9bf38
Janepop Atirattanachai <jane3030@gmail.com>
2020-06-11 21:10:43 +0700

    - change memory_limit to 2G - enable opache config

--- nginx-php7.3-fpm/start_config_php.sh
+++ nginx-php7.3-fpm/start_config_php.sh
@@ -2,7 +2,8 @@
 if [ "$STORE_SESSION" = redis ] ; then
     sed -i -e "s/session\.save_handler\ =\ files/session\.save_handler\ =\ redis/g" /etc/php/7.3/fpm/php.ini
     sed -i -e "s/\;session\.save_path\ =\ \"\/var\/lib\/php\/sessions\"/session\.save_path\ =\ \"tcp:\/\/$REDIS_HOST\"/g" /etc/php/7.3/fpm/php.ini
-    
     cat /etc/php/7.3/fpm/php.ini | grep session.save_handler
     cat /etc/php/7.3/fpm/php.ini | grep session.save_path
 fi
+sed -i -e "s/memory_limit\ =\ 128M/memory_limit\ =\ 2G/g" /etc/php/7.3/fpm/php.ini
+sed -i -e "s/\;opcache\.enable=1/opcache\.enable=1/g" /etc/php/7.3/fpm/php.ini
